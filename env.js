d3f943fd0edf458b9a4105c5afd92770556513a6
0mg <millig@outlook.jp>
2014-10-20 17:46:12 +0900

    dev: depends off official tw-minus.heroku URL

--- env.js
+++ env.js
@@ -23,11 +23,6 @@ F.realfilenames = [
 // Location of tw-minus
 module.exports.L = L = {};
 L.twMinusIsOnWeb = "PORT" in process.env;
-L.PORT = process.env.PORT | 0;
-L.TW_MINUS_URL = "https://tw-minus.herokuapp.com";
+L.PORT = L.twMinusIsOnWeb ? process.env.PORT | 0 : 3000;
+L.TW_MINUS_URL = process.env.URL || "http://localhost:" + L.PORT;
 L.TW_API_URL = "https://api.twitter.com";
-
-if (!L.twMinusIsOnWeb) {
-  L.PORT = 3000;
-  L.TW_MINUS_URL = "http://localhost:" + L.PORT;
-}
